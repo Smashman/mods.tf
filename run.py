7a4f0d23d624a46658d59171a364bee29e3d18a6
Ben <ben@smash.mn>
2014-06-18 00:49:47 +0100

    Update run.py in an attempt to fix issues

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from app import app as application
-
-if __name__ == '__main__':
-    application.run()
+#!flask/bin/python
+# -*- coding: utf-8 -*-
+from app import app
+if __name__ == "__main__":
+    app.run()
