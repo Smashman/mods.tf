bf8975354a2aea7e63fb790c9c9a8cff2e4f7ce5
Ben <ben@smash.mn>
2014-07-29 23:13:57 +0100

    Remove unneeded or unused settings

--- instance/settings_example.py
+++ instance/settings_example.py
@@ -1,4 +1,3 @@
-DEBUG = False
 SECRET_KEY = ''  # Key for forms (can be anything)
 SQLALCHEMY_DATABASE_URI = ''  # URI for SQL Database
 STEAM_API_KEY = ''  # http://steamcommunity.com/dev/apikey
@@ -6,5 +5,4 @@ UPLOADED_WORKSHOPZIPS_DEST = ''  # Folder for zips to be uploaded to
 UPLOADED_MODIMAGES_DEST = ''  # Folder for images to be uploaded to (current unused)
 OUTPUT_FOLDER_LOCATION = ''  # Save location for mod files
 VPK_BINARY_PATH = ''  # VPK binaries
-SENTRY_ENABLED = True
 SENTRY_DSN = ''  # Sentry API key for logging errors
