848cf577b2f93b86dd20ec1fae943276cd5fe54e
Ben <ben@smash.mn>
2014-11-29 21:49:09 +0000

    It's not been Halloween for a while...

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ SASS_STYLE = 'compressed'
 SASS_BIN = '/usr/local/bin/sass'
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 GAME = "tf2"
-THEME = "halloween"
+#THEME = "halloween"
 DATE_STRING_FORMAT = "%d %b %Y, %H:%M UTC"
 BG_NUM = 3
