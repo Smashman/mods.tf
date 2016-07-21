0ebaf0c6be46bd2fd23b2f83145637d51e8e23f4
Ben Williams <ben@smash.mn>
2016-07-22 00:18:13 +0100

    Only two Frontline backgrounds

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ SASS_STYLE = 'compressed'
 SASS_BIN = '/usr/local/bin/sass'
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 GAME = "tf2"
-THEME = "frontline"
 DATE_STRING_FORMAT = "%d %b %Y, %H:%M UTC"
-BG_NUM = 3
+# THEME
+THEME = "frontline"
+BG_NUM = 2
