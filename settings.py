d84478b7c353038e35a1734343a8c0594c08fc0a
Ben Williams <ben@smash.mn>
2017-04-21 16:08:38 +0100

    Back to the default theme after 9 months

--- settings.py
+++ settings.py
@@ -3,6 +3,5 @@ SASS_BIN = '/usr/local/bin/sass'
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 GAME = "tf2"
 DATE_STRING_FORMAT = "%d %b %Y, %H:%M UTC"
-# THEME
-THEME = "frontline"
-BG_NUM = 2
+# THEME = "frontline"
+# BG_NUM = 2
